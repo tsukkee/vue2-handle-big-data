25c121c9b8132ac7b097077ebe02b60c89c1df30
tsukkee <takayuki0510@gmail.com>
2021-08-25 14:42:24 +0900

    apply VirtualScroll

--- src/views/Data.vue
+++ src/views/Data.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    <h1>Data</h1>
-    <button @click="loadData">データロード</button>
+  <div class="view">
+    <div class="header">
+      <h1>Data</h1>
+      <button @click="loadData">データロード</button>
+    </div>
 
     <div class="tag-selector">
       <label v-for="tag in tags" :key="tag">
@@ -10,6 +12,29 @@
       </label>
     </div>
 
+    <RecycleScroller
+      class="scroller"
+      :items="convertedData"
+      :item-size="32"
+      key-field="id"
+      v-slot="{ item }"
+    >
+      <div class="data-view">
+        <span class="name">{{ item.name }} ({{ item.count }})</span>
+        <div class="tags">
+          <span
+            v-for="tag in item.tags"
+            :key="tag.name"
+            class="tag"
+            :class="{ selected: tag.selected }"
+          >
+            [ {{ tag.name }} ]
+          </span>
+        </div>
+      </div>
+    </RecycleScroller>
+
+    <!--
     <div v-for="d in convertedData" :key="d.id" class="data-view">
       <span class="name">{{ d.name }} ({{ d.count }})</span>
       <div class="tags">
@@ -23,6 +48,7 @@
         </span>
       </div>
     </div>
+    -->
   </div>
 </template>
 
@@ -67,6 +93,12 @@ export default class DataView extends Vue {
 </script>
 
 <style lang="scss" scoped>
+.view {
+  display: flex;
+  flex-direction: column;
+  height: calc(100vh - 100px);
+}
+
 .tag-selector {
   margin: 20px 0;
 }
@@ -74,15 +106,17 @@ export default class DataView extends Vue {
 .data-view {
   display: flex;
   justify-content: flex-start;
+  align-items: center;
+  height: 32px;
+  margin: 0 40px;
 
   .name {
     width: 180px;
     text-align: left;
+    margin-right: auto;
   }
 
   .tags {
-    marign-left: auto;
-
     .tag.selected {
       color: red;
       font-weight: bold;
