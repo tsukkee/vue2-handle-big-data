68e1701fad0a5b8375148d1fce230dfcd15be84f
tsukkee <takayuki0510@gmail.com>
2021-08-25 14:16:18 +0900

    add vue-virtual-scroller

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueVirtualScroller from "vue-virtual-scroller";
+import "vue-virtual-scroller/dist/vue-virtual-scroller.css";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueVirtualScroller);
 
 new Vue({
   router,
